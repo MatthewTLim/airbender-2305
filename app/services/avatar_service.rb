ed47e3ee29a923ea78dffe677644900ea77aa8a5
Matthew Lim <matlim200@gmail.com>
2023-09-08 09:35:25 -0700

    feat: create avatar service

---
+++ app/services/avatar_service.rb
@@ -0,0 +1,14 @@
+class AvatarService
+  def get_character_affiliation(element)
+    get_url("/api/v1/characters?name=#{element}")
+  end
+
+  def get_url(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def conn
+    Faraday.new(url: 'https://last-airbender-api.fly.dev/')
+  end
+end
